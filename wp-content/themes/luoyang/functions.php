1c50e6b1bfbaf619562f38e9dc66bb661ec717a0
[Rayanuc] <[rayhanctg4@gmail.com]>
2020-07-24 15:44:04 +0600

    8.4 - Enqueue the required stylesheet and JavaScript files and create header and footer files
    8.4 - Enqueue the required stylesheet and JavaScript files and create header and footer files

--- wp-content/themes/luoyang/functions.php
+++ wp-content/themes/luoyang/functions.php
@@ -1,8 +1,8 @@
 <?php
 
-use HasinHayder\WPHelper\Modules\NavMenu;
-use HasinHayder\WPHelper\Modules\Metabox;
-use HasinHayder\WPHelper\Modules\SinglePost;
+// use HasinHayder\WPHelper\Modules\NavMenu;
+// use HasinHayder\WPHelper\Modules\Metabox;
+// use HasinHayder\WPHelper\Modules\SinglePost;
 
 require_once "inc/wphelper/vendor/autoload.php";
 require_once get_theme_file_path( 'inc/customizer/kirki_installer.php' );
@@ -54,3 +54,35 @@ function lwhhb_theme_init() {
 }
 
 add_action('after_setup_theme','lwhhb_theme_init');
+
+function lwhhl_scripts(){
+    wp_enqueue_style('luoyang-google-fonts','//fonts.googleapis.com/css2?family=Heebo:wght@300;400&family=Playfair+Display:wght@400;500;700&display=swap');
+    wp_enqueue_style('bootstrap-css',get_theme_file_uri('assets/vendor/bootstrap/css/bootstrap.min.css'));
+    wp_enqueue_style('fontawesome-css',get_theme_file_uri('assets/vendor/fontawesome/css/all.min.css'));
+    wp_enqueue_style('luoyang-custom-icon',get_theme_file_uri('assets/vendor/custom-icon/style.css'));
+    wp_enqueue_style('magnific-css',get_theme_file_uri('assets/vendor/magnific-popup/magnific-popup.css'));
+    wp_enqueue_style('owlcarousel-css',get_theme_file_uri('assets/vendor/owl/assets/owl.carousel.min.css'));
+    wp_enqueue_style('owlcarousel-assets-css',get_theme_file_uri('assets/vendor/owl/assets/owl.theme.default.min.css'));
+    wp_enqueue_style('luoyang-css',get_theme_file_uri('assets/css/main.css'));
+    wp_enqueue_style('luoyang-css',get_stylesheet());
+
+
+    wp_enqueue_script('imagesloaded-js', get_theme_file_uri('assets/vendor/imagesloaded.js'), array('jquery'), time(), true);
+    wp_enqueue_script('isotope-js', get_theme_file_uri('assets/vendor/jquery.isotope.js'), array('jquery'), time(), true);
+    wp_enqueue_script('bootstrap-js', get_theme_file_uri('assets/vendor/bootstrap/js/bootstrap.min.js'), array('jquery'), time(), true);
+    wp_enqueue_script('popper-js', get_theme_file_uri('assets/vendor/popper.min.js'), array('jquery'), time(), true);
+    wp_enqueue_script('wow-js', get_theme_file_uri('assets/vendor/wow.min.js'), array('jquery'), time(), true);
+    wp_enqueue_script('owlcarousel-js', get_theme_file_uri('assets/vendor/owl/owl.carousel.min.js'), array('jquery'), time(), true);
+    wp_enqueue_script('magnific-js', get_theme_file_uri('assets/vendor/magnific-popup/jquery.magnific-popup.min.js'), array('jquery'), time(), true);
+    wp_enqueue_script('luoyang-js', get_theme_file_uri('assets/js/scripts.js'), array('jquery','wow-js'), time(), true);
+}
+add_action('wp_enqueue_scripts','lwhhl_scripts');
+
+
+
+/* <!--basic scripts-->
+<script src="assets/vendor/visible.js"></script>
+<script src="assets/vendor/jquery.easing.1.3.js"></script>
+
+<!--basic scripts initialization-->
+<script src="assets/js/scripts.js"></script> */
